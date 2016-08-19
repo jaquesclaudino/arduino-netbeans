9a911fac7993c94766e677994e02622a68ae2c42
ericsonjoseph <ericsonjoseph@gmail.com>
2016-08-18 19:43:40 -0500

    Add format in OSValidator

--- src/atmel-plugin/src/atmel/OSValidator.java
+++ src/atmel-plugin/src/atmel/OSValidator.java
@@ -1,7 +1,7 @@
 package atmel;
 
 /**
- *
+ * OS Validator
  * @author ericson
  */
 public class OSValidator {
@@ -9,25 +9,21 @@ public class OSValidator {
     private static final String OS = System.getProperty("os.name").toLowerCase();
 
     public static boolean isWindows() {
-
         return (OS.contains("win"));
 
     }
 
     public static boolean isMac() {
-
         return (OS.contains("mac"));
 
     }
 
     public static boolean isUnix() {
-
         return (OS.contains("nix") || OS.contains("nux") || OS.contains("aix"));
 
     }
 
     public static boolean isSolaris() {
-
         return (OS.contains("sunos"));
 
     }
